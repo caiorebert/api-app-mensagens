b91bffcb2e75e327ffa99b8cbd03135233882dca
caiorebert <77111117+caiorebert@users.noreply.github.com>
2022-10-28 13:25:39 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,7 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-
+ini_set('display_errors', 1);
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
